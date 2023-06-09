ffad3fa418521af61131a034c033937e72bdbd5f
Ziyueqi-V <1208896581@qq.com>
2023-06-10 07:26:53 +0800

    change @match

--- src/header.js
+++ src/header.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  better Juejin experience, remove ads and other useless things, make it clean and simple, just like a monkey!
 // @author       Sherlock-V
-// @match        https://juejin.cn/post/7189902077554851897
+// @match        https://juejin.cn
+// @match        https://juejin.cn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=juejin.cn
 // @grant        GM_addStyle
 // @run-at       document-body
