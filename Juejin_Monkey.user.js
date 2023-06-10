d3516d53c895acc263b5e0a2e9af93fddfa3a396
Ziyueqi-V <1208896581@qq.com>
2023-06-10 11:48:26 +0800

    版本号: 0.0.43

--- Juejin_Monkey.user.js
+++ Juejin_Monkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Junjin Monkey - 掘金猴
 // @namespace    http://tampermonkey.net/
-// @version      0.0.42
+// @version      0.0.43
 // @description  better Juejin experience, remove ads and other useless things, make it clean and simple, just like a monkey!
 // @author       Sherlock-V
 // @match        https://juejin.cn
