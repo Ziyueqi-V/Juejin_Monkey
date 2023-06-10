d6ca23cc6dfd19ded0ee4b6466aa7fa1f25a7e7d
Ziyueqi-V <1208896581@qq.com>
2023-06-10 11:51:14 +0800

    版本号: 0.0.46

--- commitAll.js
+++ commitAll.js
@@ -9,11 +9,11 @@ console.log('当前包的版本:', currentVersion);
 const commitMessage = `版本号: ${currentVersion}`;
 exec('git add . && echo \'111 \' && git commit -m "' + commitMessage + '"&& echo \'222 \' ', (error, stdout, stderr) => {
   if (error) {
-    console.error(`提交代码时出现错误: ${error}`);
+    console.error(`提交代码时出现错误1: ${error}`);
     return;
   }
   if (stderr) {
-    console.error(`提交代码时出现错误: ${stderr}`);
+    console.error(`提交代码时出现错误2: ${stderr}`);
     return;
   }
   console.log('代码提交成功');
