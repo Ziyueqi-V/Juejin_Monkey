1c185544a31cae7139ad141aa06e8b63bda74e21
Ziyueqi-V <1208896581@qq.com>
2023-06-10 10:13:27 +0800

    'v${$npm_package_version}'

--- bump.js
+++ bump.js
@@ -4,4 +4,4 @@ const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 const content = fs.readFileSync('./src/header.js', 'utf8')
 const newContent = content.replace(/\/\/ @version\s+.*$/m, `// @version      ${version}`)
 fs.writeFileSync('./src/header.js', newContent, 'utf8')
-console.log('bump version');
+console.log('bump version]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]');
